a1ea47165ff4a7229626c8ef9244b86ca5d8fa2b
Minira Samadova <samadova.minira@gmail.com>
2019-08-18 16:30:41 +0900

    migrating to react

--- README.md
+++ README.md
@@ -1,68 +1,16 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Argus description
 
-## Available Scripts
+Did you know that Argus is a many-eyed giant in Greek mythology?
 
-In the project directory, you can run:
+Named after his predecessor, Argus chrome extension does almost the same - he will let you to track time spent on a particular website by following and watching your active tabs. 
 
-### `npm start`
+# Getting started
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+While on a website you want to track, click on the Argus icon located in the tab menu. 
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+In the opened popup window you'll see two buttons 'Start Tracking' and 'Clear Data'.
 
-### `npm test`
+Click 'Start Tracking' and website tracking will be initiated. 
+The tracking data will be visible in a form of pie chart and updated each time you refresh popup window. 
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Once you're done spying on yourself for a day, you can clear all data by clicking 'Clear Data' button.
---
+++ public/background.js
@@ -0,0 +1,179 @@
+/* Handling Messages From Popup */
+//listen for messages from popup.js
+chrome.runtime.onMessage.addListener(onMessage);
+
+//depending on message type decide what action to take
+function onMessage(message, sender, sendResponse) {
+  switch (message.type) {
+  case "START_TRACKING":
+  {
+    handleStartTracking(message);
+    break;
+  }
+  case "GET_TRACKING_DATA":
+  {
+    handleShowTrackingData().then(sendResponse);
+    return true;
+  }
+  case "CLEAR_TRACKING_DATA":
+  {
+    handleClearData();
+  }
+  }
+}
+/* END Handling Messages From Popup */
+
+/* Necessary Variables Declaration */
+let websites = {};
+let currentlyTrackedDomain = {
+  domain: null,
+  windowId: null
+};
+let startTracking = null;
+
+/* END Necessary Variables Declaration */
+
+/* Functions for Argus Chrome Extension */
+//get the domain from URL
+function getDomainFromUrl(url) {
+  if (url.includes("chrome://")) {
+    return url;
+  } else {
+    const matches = url.match(/^(https?:\/\/[^/]+)/);
+    return matches[1];
+  }
+}
+
+//promisified function to send data to chrome storage
+function sendDataToStorage(data) {
+  return new Promise(resolve => {
+    chrome.storage.sync.set(data, function() {
+      resolve("data was uploaded successfully");
+    });
+  });
+}
+
+//promisified function to get data from chrome storage
+function getDataFromStorage() {
+  return new Promise(resolve => {
+    chrome.storage.sync.get(null, function(result) {
+      resolve(result);
+    });
+  });
+}
+
+//execute in case "track" button was clicked
+async function handleStartTracking(message) {
+  let domain = getDomainFromUrl(message.url);
+
+  /* eslint-disable-next-line require-atomic-updates */
+  websites = await getDataFromStorage();
+  /* eslint-disable-next-line require-atomic-updates */
+  if (websites[domain] === undefined) {
+    currentlyTrackedDomain.domain = domain;
+    currentlyTrackedDomain.windowId = message.windowId;
+    startTracking = new Date();
+    /* eslint-disable-next-line require-atomic-updates */
+    websites[domain] = 0;
+    await sendDataToStorage(websites);
+  } else {
+    alert("You are already tracking this website");
+  }
+}
+
+//stop tracking in case we changed active tab and count tracking time
+async function handleStopTracking() {
+  if (currentlyTrackedDomain.domain !== null) {
+  /* eslint-disable-next-line require-atomic-updates */
+    websites = await getDataFromStorage();
+    /* eslint-disable-next-line require-atomic-updates */
+    websites[currentlyTrackedDomain.domain] += Math.floor((new Date() - startTracking) / 1000);
+    //don't forget to clear the value
+    /* eslint-disable-next-line require-atomic-updates */
+    currentlyTrackedDomain.domain = null;
+    await sendDataToStorage(websites);
+  }
+}
+
+//check in case we changed to the tab that is already tracked
+async function handleChangedToTrackedDomain(domain) {
+  /* eslint-disable-next-line require-atomic-updates */
+  websites = await getDataFromStorage();
+  if (websites[domain] !== undefined) {
+    currentlyTrackedDomain.domain = domain;
+    startTracking = new Date();
+  }
+}
+
+//Show Tracking Data
+async function handleShowTrackingData() {
+  return await getDataFromStorage();
+}
+
+//Delete all storage data when Clear button is clicked
+function handleClearData() {
+  chrome.storage.sync.clear();
+}
+/* END Functions for Argus Chrome Extension */
+
+
+/* Argus Implementation */
+chrome.tabs.onActivated.addListener(activeTabChange);
+
+//take actions in case active tab is changed
+function activeTabChange(activeInfo) {
+  //check and take actions in case if changed from tracked tab
+  //if statement is updated to eliminate race condition problem in case of window and tab change at the same time
+  if (currentlyTrackedDomain.domain !== null && currentlyTrackedDomain.windowId === activeInfo.windowId) {
+    handleStopTracking();
+  }
+
+  chrome.tabs.get(activeInfo.tabId, isCurrentTabTracked);
+
+  //if we open a tab which we already track
+  function isCurrentTabTracked(tab) {
+    const domain = getDomainFromUrl(tab.url);
+    handleChangedToTrackedDomain(domain);
+  }
+}
+
+//check if the domain within the tracked tab is changed
+chrome.tabs.onUpdated.addListener(handleTabsDomainChange);
+
+//react to domain change in the tracked tab
+async function handleTabsDomainChange(tabId, changeInfo, tab) {
+  let currentTabDomain = getDomainFromUrl(tab.url);
+  /* eslint-disable-next-line require-atomic-updates */
+  websites = await getDataFromStorage();
+  if (currentlyTrackedDomain.domain !== null && websites[currentTabDomain] === undefined) {
+    handleStopTracking();
+  } else {
+    handleChangedToTrackedDomain(currentTabDomain);
+  }
+}
+
+//listen in case the tab we are tracking is opened in another window
+chrome.windows.onFocusChanged.addListener(windowChange);
+/* eslint-disable-next-line no-unused-vars */
+function windowChange(windowId) {
+  if (currentlyTrackedDomain.domain !== null) {
+    handleStopTracking();
+  }
+
+  //necessary parameter to get access to tabs
+  let getInfo = {
+    populate: true
+  };
+
+  //check the current tab in currently active window
+  chrome.windows.getCurrent(getInfo, getCurrentTab);
+
+  function getCurrentTab(window) {
+    //check and take actions if we changed to tab we don't track
+    let currentTab = window.tabs.find(tab => tab.active === true);
+    const domain = getDomainFromUrl(currentTab.url);
+
+    handleChangedToTrackedDomain(domain);
+  }
+}
+/* END Argus Implementation */
