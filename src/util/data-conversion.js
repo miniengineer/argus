f59890282eaa632e55c503a6b3569bc7a9f47b6c
samadova.minira <minira.samadova@syncthought.com>
2019-08-22 17:53:39 +0900

    FINALLY done with tooltip customize

--- src/util/data-conversion.js
+++ src/util/data-conversion.js
@@ -1,17 +1,21 @@
+import { editTimeForTooltip } from './moment-js-imitation.js';
+
+//convert data from chrome storage
 export const convertDataForChartJs = (obj) => {
   const data = {
     labels: [],
     datasets: [{
       data: [],
       backgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56'
-      ],
-      hoverBackgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56'
+        "#4dc9f6",
+        "#f67019",
+        "#f53794",
+        "#537bc4",
+        "#acc236",
+        "#166a8f",
+        "#00a950",
+        "#58595b",
+        "#8549ba"
       ]
     }]
   };
@@ -22,3 +26,18 @@ export const convertDataForChartJs = (obj) => {
   return data;
 };
 
+//convert data from chrome storage for chartjs tooltip using the momentjs imitation function
+export const convertDataForChartJsTooltip = () => {
+  const options = {
+    tooltips: {
+      callbacks: {
+        label: function(tooltipItem, data) {
+            console.log(tooltipItem, data);
+            var label = editTimeForTooltip(data.datasets[0].data[tooltipItem.index]);
+            return label;
+        }
+      }
+    }
+  }
+  return options;
+}
